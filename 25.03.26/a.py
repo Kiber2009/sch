cc64f93e4b66a40b5b52ec4502a5a63061ee5513
Kiber <92222946+Kiber2009@users.noreply.github.com>
2026-03-24 16:51:12 +0300

    Fix string quotes in main check

--- 25.03.26/a.py
+++ 25.03.26/a.py
@@ -3,5 +3,5 @@ def a(n1: int | float, n2: int | float, n3: int | float) -> tuple[int | float, i
     return n1, n2, n3
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(*a(*map(int, input("Введите три натуральных числа:\n").split())))
